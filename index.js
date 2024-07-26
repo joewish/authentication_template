f7b7b92c039b39eba8e1f6219bcc570300126bda
joewish <72031491+joewish@users.noreply.github.com>
2024-07-26 13:06:28 +0000

    google authantication 2

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import express, { urlencoded }  from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import path from "path";
 import expressEjsLayouts from "express-ejs-layouts";
 import bodyParser from "body-parser";
-import {router} from "./src/routes/app.routes.js";
-const configPath = path.resolve("src","config","uat.env")
-dotenv.config({path:configPath})
+import { router } from "./src/routes/app.routes.js";
+const configPath = path.resolve("src", "config", "uat.env");
+dotenv.config({ path: configPath });
 const app = express();
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-console.log(path.join("public"))
-app.use("/",express.static(path.join('public')));
+console.log(path.join("public"));
+app.use("/", express.static(path.join("public")));
 app.set("view engine", "ejs");
-app.set("views", path.resolve("src","views"));
+app.set("views", path.resolve("src", "views"));
 // configure routes
-app.use("/",router);
+app.use("/", router);
 
 export default app;
--- src/config/recaptcha.js
+++ src/config/recaptcha.js
@@ -1,7 +1,19 @@
-import recaptcha from 'recaptcha2';
+import recaptcha from "recaptcha2";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
+import GoogleStrategy from "passport-google-oauth20";
+import findOrCreate from "mongoose-findorcreate";
 
 export const recaptcha2 = new recaptcha({
   siteKey: process.env.RECAPTCHA_SITE_KEY,
-  secretKey: process.env.RECAPTCHA_SECRET_KEY
+  secretKey: process.env.RECAPTCHA_SECRET_KEY,
 });
 
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
