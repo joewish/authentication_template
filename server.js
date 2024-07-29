fb02e05ccf0c2488a771c4917846490beb2f7b9e
joewish <72031491+joewish@users.noreply.github.com>
2024-07-29 14:11:34 +0000

    handling other pages-2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const serverStar = app.listen(process.env.PORT, async (err) => {
   if (err) {
     console.log(`server failed with error ${err}`);
   } else {
-    await connectDB();
+    // await connectDB();
     console.log(`server is running at http://localhost:${process.env.PORT}`);
   }
 });
--- src/controller/app.controller.js
+++ src/controller/app.controller.js
@@ -1,76 +1,77 @@
-import { userSchema } from '../model/app.schema.js';
-import nodemailer from 'nodemailer';
-import bcrypt from 'bcryptjs';
-import crypto from 'crypto';
-import {transporter} from '../config/nodemailer.js';
-import mongoose from 'mongoose';
-const User = mongoose.model("user", userSchema)
+import { userSchema } from "../model/app.schema.js";
+import nodemailer from "nodemailer";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import { transporter } from "../config/nodemailer.js";
+import mongoose from "mongoose";
+const User = mongoose.model("user", userSchema);
 
 export const getSignup = (req, res, next) => {
-  res.status(201).render('landing');
+  res.status(201).render("landing");
 };
 
 export const postSignup = async (req, res) => {
-  const { name,email, password} = req.body;
+  const { name, email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser.name==name) {
-      if (existingUser.password==password) {
-        res.status(200).render('home',{error:existingUser.email})
-      }else{
-        res.status(200).render('signin',{error:true})
+    if (existingUser.name == name) {
+      if (existingUser.password == password) {
+        res.status(200).render("home", { error: existingUser.email });
+      } else {
+        res.status(200).render("signin", { error: true });
       }
       //return res.render('home',{user:existingUser.name});
-    }else{
-      const newUser = new User({ name,email, password });
+    } else {
+      const newUser = new User({ name, email, password });
       await newUser.save();
-      return res.render('home',{user:existingUser.name});
+      return res.render("home", { user: existingUser.name });
     }
   } catch (err) {
     console.error(err.message);
-    return res.render('error');
+    return res.render("error");
   }
 };
 
 export const getSignin = (req, res) => {
-  res.render('signin',{error:false});
+  res.render("signin", { error: false });
 };
 
 export const signout = (req, res) => {
   req.logout();
-  res.render('signin',{error:false});
+  res.render("signin", { error: false });
 };
 
 export const getHome = (req, res) => {
-  res.render('home', { user: req.user });
+  res.render("home", { user: req.user });
 };
 
 export const getResetPassword = (req, res) => {
-  res.render('resetPassword');
+  res.render("resetPassword");
 };
 
 export const postResetPassword = async (req, res) => {
+  console.log(req.body);
   const { email, newPassword, newPassword2 } = req.body;
   if (newPassword !== newPassword2) {
-    return res.render('/reset');
+    return res.render("resetPassword");
   }
 
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.render('/reset');
+      return res.render("resetPassword");
     }
 
     user.password = newPassword;
     await user.save();
-    res.render('signin',{error:false});
+    res.render("signin", { error: false });
   } catch (err) {
-    res.render('/reset');
+    res.render("resetPassword");
   }
 };
 
 export const getForgotPassword = (req, res) => {
-  res.render('forgotPassword');
+  res.render("forgotPassword");
 };
 
 export const postForgotPassword = async (req, res) => {
@@ -78,10 +79,10 @@ export const postForgotPassword = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.render('/forgot');
+      return res.render("/forgot");
     }
 
-    const token = crypto.randomBytes(20).toString('hex');
+    const token = crypto.randomBytes(20).toString("hex");
     user.resetPasswordToken = token;
     user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
     await user.save();
@@ -89,19 +90,19 @@ export const postForgotPassword = async (req, res) => {
     const mailOptions = {
       to: user.email,
       from: process.env.EMAIL_USER,
-      subject: 'Password Reset',
+      subject: "Password Reset",
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
       Please click on the following link, or paste this into your browser to complete the process:\n\n
       http://${req.headers.host}/reset/${token}\n\n
-      If you did not request this, please ignore this email and your password will remain unchanged.\n`
+      If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
     await transporter.sendMail(mailOptions);
 
     // req.flash('info', `An email has been sent to ${user.email} with further instructions.`);
-    res.render('/forgot');
+    res.render("/forgot");
   } catch (err) {
     // req.flash('error', 'Error sending the email');
-    res.render('/forgot');
+    res.render("/forgot");
   }
 };
