5f6761aa964991585c24a002d265418a83c63711
joewish <72031491+joewish@users.noreply.github.com>
2024-07-31 00:53:53 +0530

    adding styling 2

--- src/controller/app.controller.js
+++ src/controller/app.controller.js
@@ -1,11 +1,11 @@
 import { userSchema } from "../model/app.schema.js";
 import nodemailer from "nodemailer";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import crypto from "crypto";
 import { sendPasswordResetEmail } from "../config/nodemailer.js";
 import mongoose from "mongoose";
 const User = mongoose.model("user", userSchema);
-
+const url="http://localhost:3000"
 export const getSignup = (req, res, next) => {
   res.status(201).render("landing");
 };
@@ -14,31 +14,30 @@ export const postSignup = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser.name == name) {
-      if (existingUser.password == password) {
-        res.status(200).render("home", { error: existingUser.email });
+    if (existingUser) {
+      const isPasswordMatch = await bcrypt.compare(password, existingUser.password);
+      if (existingUser.name === name && isPasswordMatch) {
+        return res.status(200).render("home", { user: existingUser});
       } else {
-        res.status(200).render("signin", { error: true });
+        return res.status(200).render("signin", { error: true });
       }
-      //return res.render('home',{user:existingUser.name});
     } else {
-      const newUser = new User({ name, email, password });
+      const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
+      const newUser = new User({ name, email, password: hashedPassword });
       await newUser.save();
-      return res.render("home", { user: existingUser.name });
+      return res.render("home", { user: newUser.name });
     }
   } catch (err) {
-    console.error(err.message);
-    return res.render("error");
+        return res.render("error");
   }
 };
-
 export const getSignin = (req, res) => {
   res.render("signin", { error: false });
 };
 
 export const signout = (req, res) => {
   req.logout();
-  res.render("signin", { error: false });
+  res.render("landing", { error: false });
 };
 
 export const getHome = (req, res) => {
@@ -78,14 +77,22 @@ export const postForgotPassword = async (req, res) => {
   const { email } = req.body;
   try {
     const user = await User.findOne({ email });
-    if (!user) {
-      return res.render("/forgot");
-    }
+    // if (!user) {
+    //   return res.render("forgot");
+    // }
 
     const token = crypto.randomBytes(20).toString("hex");
     user.resetPasswordToken = token;
     user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
     await user.save();
+    const transporter = nodemailer.createTransport({
+      service: process.env.SMPT_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+  
 
     const mailOptions = {
       to: user.email,
@@ -93,7 +100,7 @@ export const postForgotPassword = async (req, res) => {
       subject: "Password Reset",
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
       Please click on the following link, or paste this into your browser to complete the process:\n\n
-      http://${req.headers.host}/reset/${token}\n\n
+      ${url}/reset
       If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
