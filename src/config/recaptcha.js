eebcd90531876ac14eb333b2311af41f5d887cd6
joewish <72031491+joewish@users.noreply.github.com>
2024-08-07 15:35:57 +0000

    changed the redirect URL

--- src/config/recaptcha.js
+++ src/config/recaptcha.js
@@ -1,28 +1,27 @@
 import dotenv from "dotenv";
-import path from "path"
+import path from "path";
 const configPath = path.resolve("src", "config", "uat.env");
 dotenv.config({ path: configPath });
-import passport  from "passport";
+import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import {RecaptchaEnterpriseServiceClient} from '@google-cloud/recaptcha-enterprise';
+import { RecaptchaEnterpriseServiceClient } from "@google-cloud/recaptcha-enterprise";
 export const passport2 = passport;
-//initialize 
+//initialize
 passport2.use(
   new GoogleStrategy(
     {
-
       clientID: process.env.GOOGLE_CLIENT_ID, // google client id
       clientSecret: process.env.GOOGLE_CLIENT_SECRET, // google client secret
       // the callback url added while creating the Google auth app on the console
-      callbackURL: "http://localhost:3000/auth/google/callback", 
+      callbackURL:"https://authentication-template.onrender.com/auth/google/callback",
       passReqToCallback: true,
     },
 
-// returns the authenticated email profile
- async function (request, accessToken, refreshToken, profile, done) {
-     return done(null, profile);
-    }
-  )
+    // returns the authenticated email profile
+    async function (request, accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    },
+  ),
 );
 
 // function to serialize a user/profile object into the session
@@ -35,13 +34,13 @@ passport2.deserializeUser(function (user, done) {
   done(null, user);
 });
 /**
-  * Create an assessment to analyse the risk of a UI action.
-  *
-  * projectID: Your Google Cloud project ID.
-  * recaptchaSiteKey: The reCAPTCHA key associated with the site/app
-  * token: The generated token obtained from the client.
-  * recaptchaAction: Action name corresponding to the token.
-  */
+ * Create an assessment to analyse the risk of a UI action.
+ *
+ * projectID: Your Google Cloud project ID.
+ * recaptchaSiteKey: The reCAPTCHA key associated with the site/app
+ * token: The generated token obtained from the client.
+ * recaptchaAction: Action name corresponding to the token.
+ */
 async function createAssessment({
   // TO-DO: Replace the token and reCAPTCHA action variables before running the sample.
   projectID = "authenticator-1722679430033",
@@ -55,7 +54,7 @@ async function createAssessment({
   const projectPath = client.projectPath(projectID);
 
   // Build the assessment request.
-  const request = ({
+  const request = {
     assessment: {
       event: {
         token: token,
@@ -63,13 +62,15 @@ async function createAssessment({
       },
     },
     parent: projectPath,
-  });
+  };
 
-  const [ response ] = await client.createAssessment(request);
+  const [response] = await client.createAssessment(request);
 
   // Check if the token is valid.
   if (!response.tokenProperties.valid) {
-    console.log(`The CreateAssessment call failed because the token was: ${response.tokenProperties.invalidReason}`);
+    console.log(
+      `The CreateAssessment call failed because the token was: ${response.tokenProperties.invalidReason}`,
+    );
     return null;
   }
 
@@ -86,7 +87,9 @@ async function createAssessment({
 
     return response.riskAnalysis.score;
   } else {
-    console.log("The action attribute in your reCAPTCHA tag does not match the action you are expecting to score");
+    console.log(
+      "The action attribute in your reCAPTCHA tag does not match the action you are expecting to score",
+    );
     return null;
   }
 }
